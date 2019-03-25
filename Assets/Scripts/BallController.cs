2b4e156d5a9c76ef7a060dd21dde4cbac58193e1
0115electro <45603148+0115electro@users.noreply.github.com>
2019-03-26 01:31:30 +0530

    ground

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Collider ball;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ball = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
